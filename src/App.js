b70e320a1da04692aa5edb83c2663f4df9593479
fabio-sv <fabio_dev@outlook.com>
2022-11-29 16:50:30 -0300

    criação do component Navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
 import Home from "./Home/Home";
 
 function App() {
     return (
         <div className="App">
             <BrowserRouter>
+                <Navbar />
                 <Routes>
                     <Route path="/" element={<Home />} />
                 </Routes>
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,35 @@
+import { Link, NavLink } from "react-router-dom";
+import styles from "./Navbar.module.css";
+
+const Navbar = () => {
+    return (
+        <nav className={styles.navbar}>
+            <div className={styles.container}>
+                <div className={styles.links}>
+                    <NavLink className={styles.brand} to={"/"}>
+                        Mini<span>Blog</span>
+                    </NavLink>
+                    <ul>
+                        <li>
+                            <NavLink to={"/"}>Home</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to={"/"}>Sobre</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to={"/"}>Novo Post</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to={"/"}>Dashboard</NavLink>
+                        </li>
+                    </ul>
+                </div>
+                <div>
+                    <input type="checkbox" /> Dark Mode
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
