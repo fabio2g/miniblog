5d7d5e73a71cdcd395e13c8e6375a3e7a4c0e6f8
fabio-sv <fabio_dev@outlook.com>
2022-11-30 11:54:15 -0300

    adicionado elementos ao footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,26 @@
+import { Link } from "react-router-dom";
 import styles from "./Footer.module.css";
 
 const Footer = ({ theme }) => {
     console.log("footer:", theme);
     return (
         <footer className={theme ? styles.footer : styles.footer_dark}>
-            <h1>Footer</h1>
+            <h3>Compartilhe o seu conhecimento em tecnologia aqui!</h3>
+            <p>Mini Blog &copy; 2022</p>
+            <div className={styles.social_media}>
+                <Link>
+                    <i class="fa-brands fa-github"></i>
+                </Link>
+                <Link>
+                    <i class="fa-brands fa-linkedin"></i>
+                </Link>
+                <Link>
+                    <i class="fa-brands fa-instagram"></i>
+                </Link>
+                <Link>
+                    <i class="fa-brands fa-codepen"></i>
+                </Link>
+            </div>
         </footer>
     );
 };
