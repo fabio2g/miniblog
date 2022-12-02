da52e00a3e5e5d6f78490c6c4f5f13e05c7165a1
fabio-sv <fabio_dev@outlook.com>
2022-12-02 13:32:48 -0300

    criado o form de postagem

--- src/pages/CreatePost/CreatePost.js
+++ src/pages/CreatePost/CreatePost.js
@@ -1,9 +1,62 @@
+import { useState } from "react";
 import styles from "./CreatePost.module.css";
 
 const CreatePost = () => {
+    const [title, setTitle] = useState("");
+    const [image, setImage] = useState("");
+    const [body, setBody] = useState("");
+    const [tags, setTags] = useState("");
+
     return (
         <div>
-            <h1>CreatePost</h1>
+            <h1>Criar post</h1>
+            <p>Escreva sobre seus interesse e compartilhe seu conhecimeto!</p>
+            <form>
+                <label>
+                    <span>Título:</span>
+                    <input
+                        type="text"
+                        name="title"
+                        required
+                        placeholder="Pense em um bom título"
+                        onChange={(e) => setTitle(e.target.value)}
+                        value={title}
+                    />
+                </label>
+                <label>
+                    <span>URL da imagem:</span>
+                    <input
+                        type="text"
+                        placeholder="Insira uma imagem que represente o seu post"
+                        required
+                        onChange={(e) => setImage(e.target.value)}
+                        value={image}
+                    />
+                </label>
+                <label>
+                    <span>Conteúdo:</span>
+                    <textarea
+                        name="body"
+                        cols="30"
+                        rows="10"
+                        placeholder="Insira o conteúdo do post"
+                        required
+                        onChange={(e) => setBody(e.target.value)}
+                        value={body}
+                    ></textarea>
+                </label>
+                <label>
+                    <span>Tags:</span>
+                    <input
+                        type="text"
+                        placeholder="Insira as tags separadas por vígula"
+                        required
+                        onChange={(e) => setTags(e.target.value)}
+                        value={tags}
+                    />
+                </label>
+                <button className="btn_success">Postar</button>
+            </form>
         </div>
     );
 };
