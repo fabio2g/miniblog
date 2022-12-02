0c6ce03d9901bdb47abad85f3d686744e88e7734
fabio-sv <fabio_dev@outlook.com>
2022-12-02 20:23:08 -0300

    definindo o conteudo da página home

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,9 +1,21 @@
+import { useFetchDocuments } from "../../hooks/useFetchDocuments";
 import styles from "./Home.module.css";
 
 const Home = () => {
+    const { documents: posts, loading, error } = useFetchDocuments("posts");
+
+    console.log(posts);
+
     return (
         <div>
-            <h1>Home</h1>
+            {posts &&
+                posts.map((post) => (
+                    <div key={post.id}>
+                        <img src={post.image} alt={post.title} />
+                        <h3>{post.title}</h3>
+                        <p>{post.tagsArray}</p>
+                    </div>
+                ))}
         </div>
     );
 };
