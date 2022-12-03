6b4a1abb1997978968b0ffe080855a9a84a98648
fabio-sv <fabio_dev@outlook.com>
2022-12-03 15:24:11 -0300

    criado o hook de fetch por id

---
+++ src/hooks/useFetchDocument.js
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from "react";
+
+import { db } from "../firebase/config";
+import { collection, getDoc } from "firebase/firestore";
+
+export const useFetchDocument = (docCollection, id) => {
+    const [document, setDocument] = useState("");
+    const [loading, setLoading] = useState("");
+    const [cancelled, setCancelled] = useState(false);
+    const [error, setError] = useState("");
+
+    useEffect(() => {
+        const loadingDoc = async () => {
+            if (cancelled) return;
+
+            setLoading(true);
+            setError("");
+
+            try {
+                const docRef = collection(db, docCollection, id);
+                const docSnap = await getDoc(docRef);
+
+                setDocument(docSnap.data());
+                setLoading(false);
+            } catch (error) {
+                setError(error.message);
+                console.log(error);
+
+                setLoading(false);
+            }
+        };
+        loadingDoc();
+    }, [docCollection, id, cancelled]);
+
+    useEffect(() => {
+        return () => setCancelled(true);
+    });
+
+    return { document, loading, error };
+};
